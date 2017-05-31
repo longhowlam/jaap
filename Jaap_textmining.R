3b2b2e8083ce0a6fc5753e4e238340884dc7763a
longhow lam <longhowlam@gmail.com>
2017-05-31 11:40:12 +0200

    Add files via upload

---
+++ Jaap_textmining.R
@@ -0,0 +1,179 @@
+library(dplyr)
+library(text2vec)
+library(stringr)
+library(glmnet)
+library(xgboost)
+
+## import scraped house data from www.jaap.nl
+jaap = readRDS("JaapResultsLongDesc20170528.RDs")
+
+## import postcode data, not really needed for text mining
+PC6 = readRDS("postocdes_NL.RDs")
+
+jaap = jaap %>% 
+  left_join(
+    PC6,
+    by = c("PC6" = "Postcode_spatie")
+  )
+
+# median prijse per city
+city = jaap %>%
+  group_by(city) %>%
+  summarise(
+    median = median(prijs, na.rm=TRUE)
+  )
+
+
+### remove empty descriptions, short descriptions outlying prices 
+jaap = jaap %>%
+  filter(
+    longdescription != ""
+  ) %>% 
+  mutate(
+    Nwoorden = str_count(longdescription, pattern = '[\\w]+'),
+    huisbeschrijving = str_to_lower(longdescription)
+  )  %>%
+  filter(
+    prijs > 50000,
+    prijs < 1250000,
+    Nwoorden > 20,
+    Nwoorden < 1500
+  )
+
+
+# create an index column
+jaap$id = 1:dim(jaap)[1]
+
+
+###########  text mining part ##############################
+
+## tokennize
+
+jaap_tokens = jaap$huisbeschrijving %>% word_tokenizer
+
+### use the tokens to create an iterator and vocabular
+iter = itoken(
+  jaap_tokens, 
+  ids = jaap$id,
+  progressbar = TRUE
+)
+
+## remove some Dutch stopwords
+stw = tm::stopwords("nl")
+
+vocab = create_vocabulary(
+  iter, 
+  stopwords = stw,
+  ngram = c(ngram_min = 1L, ngram_max = 3L)
+)
+
+pruned_vocab = prune_vocabulary(vocab, doc_proportion_min = 0.0035 )
+
+#### create document term matrix
+vectorizer = vocab_vectorizer(pruned_vocab)
+dtm = create_dtm(iter, vectorizer)
+dim(dtm)
+
+# define tfidf model
+tfidf = TfIdf$new()
+# fit model to train data and transform train data with fitted model
+dtm_tfidf = fit_transform(dtm, tfidf)
+
+
+########## fit price models ###############
+
+## split into test and train
+
+Ntrain = floor(0.8*nrow(dtm))
+tr.idx = sample(1:nrow(dtm), size = Ntrain)
+
+dtm_train = dtm[tr.idx,]
+dtm_train_tfidf = dtm_tfidf[tr.idx,]
+dim(dtm_train)
+dim(dtm_train_tfidf)
+
+dtm_test = dtm[-tr.idx,]
+dtm_test_tfidf = dtm_tfidf[-tr.idx,]
+dim(dtm_test)
+
+target_train = jaap$prijs[tr.idx]
+target_test = jaap$prijs[-tr.idx]
+
+
+#### glmnet 
+
+glmnet_prijsmodel = cv.glmnet(
+  x = dtm_train,
+  y = target_train, 
+  # L1 penalty
+  alpha = 1,
+  # high value is less accurate, but has faster training
+  thresh = 1e-4,
+  # again lower number of iterations for faster training
+  maxit = 1e4,
+  type.measure="mae"
+)
+
+plot(glmnet_prijsmodel)
+print(paste("mse =", round(min(glmnet_prijsmodel$cvm), 4)))
+
+### extract terms with 'large' negative or positive value
+
+tmp = coef(glmnet_prijsmodel)
+wordvalue = as.numeric(tmp)
+names(wordvalue) = tmp@Dimnames[[1]]
+
+### 100 'largest' and 'smallest' terms
+neg100 = sort(wordvalue)[1:100]
+pos100 = sort(wordvalue, decreasing = TRUE)[1:100]
+
+outterms = tibble::tibble(
+  NegWords = names(neg100),
+  NegValue = neg100,
+  PosWords = names(pos100),
+  PosValues = pos100
+)
+
+## R squared calculation on the hold out test set
+test_huizenprijs = predict(glmnet_prijsmodel, newx = dtm_test)[,1]
+R2 = 1 - sum((target_test - test_huizenprijs)^2) / sum((target_test - mean(target_test))^2)
+R2
+
+############# xgboost ###################################################################
+
+param = list(
+  max_depth = 10,
+  nthread = 4
+)
+
+xgbmodel = xgboost(
+  params = param,
+  data = dtm_train,
+  label = target_train,
+  nrounds = 150,
+  print_every_n = 5L
+)
+
+
+## some model diagnostics 
+varimp = xgb.importance(colnames(dtm_train), model = xgbmodel)
+head(varimp, 25)
+
+
+xgb.plot.tree(colnames(dtm_train), model = xgbmodel, n_first_tree = 3)
+
+p = xgb.plot.multi.trees(
+  model = xgbmodel,
+  feature_names = colnames(dtm_train),
+  features_keep = 30
+)
+print(p)
+
+
+## R squared calculation on the hold out test set
+test_huizenprijs = predict(xgbmodel, newdata =  dtm_test)
+R2 = 1 - sum((target_test - test_huizenprijs)^2) / sum((target_test - mean(target_test))^2)
+R2
+
+
+
